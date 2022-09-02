e3c73aeadc42a0bd031c980544069f6152594f58
ExcaliburSims <ximeonzilu@gmail.com>
2022-09-02 11:47:22 +0100

    feat: ...

--- passwordToggle/src/Login.js
+++ passwordToggle/src/Login.js
@@ -5,18 +5,37 @@ import {
   StyleSheet,
   Image,
   TouchableOpacity,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from 'react-native';
 import React from 'react';
-import Icon from 'react-native-vector-icons/Entypo';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
 const Login = () => {
   return (
-    <View style={styles.layout}>
-      <View style={styles.box1}>
-        <Text>Yando</Text>
-      </View>
+    <KeyboardAvoidingView
+      /* behavior={Platform.OS === 'android' ? 'padding' : 'height'} */
+      style={styles.layout}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.box1}>
+          <TextInput
+            placeholder="Password"
+            style={styles.textInput}
+            placeholderTextColor={'#88888888'}
+          />
+          <Icon
+            name="ios-eye-outline"
+            size={30}
+            color="#88888888"
+            style={styles.icone}
+          />
+        </View>
+      </TouchableWithoutFeedback>
       <View style={styles.box2}>
         <TouchableOpacity>
           <Image
@@ -37,7 +56,7 @@ const Login = () => {
           />
         </TouchableOpacity>
       </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -48,10 +67,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#1F172F',
   },
   box1: {
-    flex: 5,
+    flex: 3,
     backgroundColor: 'white',
     borderBottomLeftRadius: 100,
     borderBottomRightRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   box2: {
     flex: 1,
@@ -65,5 +86,21 @@ const styles = StyleSheet.create({
   Image: {
     width: 50,
     height: 50,
+  },
+  textInput: {
+    height: 40,
+    borderColor: '#000',
+    borderBottomWidth: 1,
+    marginBottom: 36,
+    color: '#000',
+    width: 250,
+    fontSize: 20,
+    //backgroundColor: 'red',
+    paddingHorizontal: 15,
+  },
+  icone: {
+    position: 'absolute',
+    right: 70,
+    top: '44%',
   },
 });
