f12abee1c24764771147498a840276487ce22504
ExcaliburSims <ximeonzilu@gmail.com>
2022-09-04 06:58:43 +0100

    feat: add safeAreaView

--- passwordToggle/App.js
+++ passwordToggle/App.js
@@ -7,10 +7,22 @@
  */
 
 import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
 import Login from './src/Login';
 
 const App = () => {
-  return <Login />;
+  return (
+    <SafeAreaView style={styles.layout}>
+      <Login />
+    </SafeAreaView>
+  );
 };
 
 export default App;
+const styles = StyleSheet.create({
+  layout: {
+    flex: 1,
+    backgroundColor: '#1F172F',
+    justifyContent: 'center',
+  },
+});
